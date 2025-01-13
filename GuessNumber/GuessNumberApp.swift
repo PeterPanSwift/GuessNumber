7763958f18064d66d012a0001d9da5cf76d7cb55
peter pan <apppeterpan@gmail.com>
2025-01-13 18:56:05 +0800

    Initial Commit

---
+++ GuessNumber/GuessNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberApp.swift
+//  GuessNumber
+//
+//  Created by SHIH-YING PAN on 2025/1/13.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
